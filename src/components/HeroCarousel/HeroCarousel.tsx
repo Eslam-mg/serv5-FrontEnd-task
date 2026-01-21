8523465fe005e2eb56c89f320ccf67890f900056
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-21 17:54:02 +0200

    add HeroCarousel component

---
+++ src/components/HeroCarousel/HeroCarousel.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from 'react';
+
+const slides = [
+    {
+        title: "SMART WEARABLE.",
+        subtitle: "Best Deal Online on smart watches",
+        discount: "UP to 80% OFF",
+        image: "/image/image2.png",
+    },
+    {
+        title: "SMART WEARABLE.",
+        subtitle: "Best Deal Online on smart watches",
+        discount: "UP to 80% OFF",
+        image: "/image/image4.png",
+    },
+    {
+        title: "SMART WEARABLE.",
+        subtitle: "Best Deal Online on smart watches",
+        discount: "UP to 80% OFF",
+        image: "/image/image7.png",
+    },
+]
+
+export default function HeroCarousel() {
+    return (
+        <div>HeroCarousel</div>
+    )
+}
