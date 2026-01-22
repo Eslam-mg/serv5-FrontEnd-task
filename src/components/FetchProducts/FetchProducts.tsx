5e5906ca7e2ff487b5e538553109a439555c09ae
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-22 11:20:48 +0200

    fetch data

--- src/components/FetchProducts/FetchProducts.tsx
+++ src/components/FetchProducts/FetchProducts.tsx
@@ -1,7 +1,24 @@
 "use client";
 import React from 'react';
+interface Product {
+    id: number
+    title: string
+    price: number
+    description: string
+    category: string
+    image: string
+    rating: {
+        rate: number
+        count: number
+    }
+}
 
 export default function FetchProducts() {
+    const getAllProducts = async (): Promise<Product[]> => {
+        const response = await fetch("https://fakestoreapi.com/products");
+        return response.json();
+    };
+
     return (
         <div>FetchProducts</div>
     )
