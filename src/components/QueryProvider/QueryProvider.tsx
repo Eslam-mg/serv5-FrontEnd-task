3ac6bc62f867ba1e3e10759eb2ea77e4070afc01
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-22 11:00:20 +0200

    useState to To ensure that QueryClient is performed only once and that the cache is maintained between re-renders, this prevents unnecessary re-feeds.

--- src/components/QueryProvider/QueryProvider.tsx
+++ src/components/QueryProvider/QueryProvider.tsx
@@ -1,11 +1,15 @@
 "use client"
-import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { useState } from 'react';
 
 interface PropsInterface {
     children : React.ReactNode
 }
 export default function QueryProvider({children}: PropsInterface) {
+    const [queryClient] = useState(()=> new QueryClient);
     return (
-        <div>QueryProvider</div>
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
     )
 }
