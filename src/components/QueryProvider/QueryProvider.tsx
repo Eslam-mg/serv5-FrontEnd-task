90aaa6577ac72ab52afb7e61dbe6ec8040299a21
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-01-22 10:51:23 +0200

    add interface to QueryProvider component

--- src/components/QueryProvider/QueryProvider.tsx
+++ src/components/QueryProvider/QueryProvider.tsx
@@ -1,7 +1,10 @@
 "use client"
 import React from 'react';
 
-export default function QueryProvider() {
+interface PropsInterface {
+    children : React.ReactNode
+}
+export default function QueryProvider({children}: PropsInterface) {
     return (
         <div>QueryProvider</div>
     )
